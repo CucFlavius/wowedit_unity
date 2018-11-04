c72624d2d30910bb5e7f7916027a9ee6f2948f0e
warsongkiller <warsongkiller.s8@gmail.com>
2018-11-04 13:08:26 +0100

    Added DBD reader.

---
+++ Assets/Data/WoW Format Parsers/DB2/DBD/DBDStructs.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DB2.DBDBuild;
+using static DB2.DBDBuildRange;
+
+public static partial class DB2
+{
+    public static class DBDStructs
+    {
+        public struct DBDefinition
+        {
+            public Dictionary<string, ColumnDefinition> columnDefinitions;
+            public VersionDefinitions[] versionDefinitions;
+        }
+
+        public struct VersionDefinitions
+        {
+            public Build[] builds;
+            public BuildRange[] buildRanges;
+            public string[] layoutHashes;
+            public string comment;
+            public Definition[] definitions;
+        }
+
+        public struct Definition
+        {
+            public int size;
+            public int arrLength;
+            public string name;
+            public bool isID;
+            public bool isRelation;
+            public bool isNonInline;
+            public bool isSigned;
+            public string comment;
+        }
+
+        public struct ColumnDefinition
+        {
+            public string type;
+            public string foreignTable;
+            public string foreignColumn;
+            public bool verified;
+            public string comment;
+        }
+    }
+}
