64e16beb46e7eff0bd1f385feb187bf0635aace4
warsongkiller <warsongkiller.s8@gmail.com>
2018-11-05 11:34:18 +0100

    Replaced SubModule with local code so we can edit it if needed

--- Assets/UI/Settings Window/SettingsWindow.cs
+++ Assets/UI/Settings Window/SettingsWindow.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SettingsWindow : MonoBehaviour {
+public class SettingsWindow : MonoBehaviour
+{
+    public enum SettingsTab
+    {
+        General,
+        Discord,
+    }
+    public GameObject[] TabPanels;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -27,5 +34,22 @@ public class SettingsWindow : MonoBehaviour {
     public void Button_Close()
     {
         gameObject.SetActive(false);
+    }
+
+    public void ClickedSettingsTab(int tab)
+    {
+        CloseAllTabs();
+
+        for (int i = 0; i < TabPanels.Length; i++)
+        {
+            
+        }
+    }
+    public void CloseAllTabs()
+    {
+        for (int i = 0; i < TabPanels.Length; i++)
+        {
+            TabPanels[i].SetActive(false);
+        }
     }
 }
