623266e8ea7e2e41fe4b0e688ce42bdd57f0a06a
CucFlavius <cucflavius@gmail.com>
2018-11-07 04:08:18 +0200

    Terrain Editing WIP

---
+++ Assets/UI/Toolboxes/TerrainEditor/TerrainEditorToolbox.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainEditorToolbox : MonoBehaviour
+{
+    public GameObject brushProjectorA;
+    public GameObject brushProjectorB;
+
+    void Awake()
+    {
+        EnableTerrainTool();
+    }
+
+    public void EnableTerrainTool()
+    {
+        brushProjectorA.SetActive(true);
+        brushProjectorB.SetActive(true);
+    }
+
+    public void DisableTerrainTool()
+    {
+        brushProjectorA.SetActive(false);
+        brushProjectorB.SetActive(false);
+    }
+
+}
---
+++ Assets/UI/Toolboxes/ToolboxManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolboxManager : MonoBehaviour
+{
+    public List<GameObject> toolboxes;
+    public List<GameObject> toolboxButtons;
+
+
+    public void OpenTerrainEditor(bool toggle)
+    {
+        if (toggle)
+            toolboxes[0].SetActive(true);
+        else
+            CloseTerrainEditor();
+    }
+
+    public void CloseTerrainEditor()
+    {
+        toolboxes[0].SetActive(false);
+        toolboxButtons[0].GetComponent<UnityEngine.UI.Toggle>().isOn = false;
+    }
+
+    public void CloseToolboxes()
+    {
+        foreach (GameObject toolbox in toolboxes)
+        {
+            toolbox.SetActive(false);
+        }
+    }
+
+}
